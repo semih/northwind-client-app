dddc43379df16c54d7da11f73a57d9eb1bc241b7
semih <semihkirdinli@gmail.com>
2021-06-14 00:28:19 +0300

    June 14, 0028

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import Dashboard from "./layouts/Dashboard";
+import "semantic-ui-css/semantic.min.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
