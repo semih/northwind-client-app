50c8da8938cabd6471aeaeae9fd7c151e813fdb6
semih <semihkirdinli@gmail.com>
2021-06-16 14:32:05 +0300

    useHistory from react-router-dom

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -3,12 +3,14 @@ import { Menu, Container } from "semantic-ui-react";
 import CartSummary from "./CartSummary";
 import SignedOut from "./SignedOut";
 import SignedIn from "./SignedIn";
+import { useHistory } from "react-router-dom";
 
 export default function Navi() {
   const [isAuthenticated, setIsAuthenticated] = useState(true);
-
+  const history = useHistory();
   function handleSignedOut() {
     setIsAuthenticated(false);
+    history.push("/");
   }
 
   function handleSignedIn() {
